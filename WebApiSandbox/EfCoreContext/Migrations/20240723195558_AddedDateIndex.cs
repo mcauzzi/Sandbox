888a194d5c55131eb66941a9bdaf28277a0b8c79
marco <furai93@gmail.com>
2024-07-23 21:57:41 +0200

    Added Date index to table WeatherForecasts

---
+++ WebApiSandbox/EfCoreContext/Migrations/20240723195558_AddedDateIndex.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EfCoreContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedDateIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_WeatherForecasts_Date",
+                table: "WeatherForecasts",
+                column: "Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_WeatherForecasts_Date",
+                table: "WeatherForecasts");
+        }
+    }
+}
